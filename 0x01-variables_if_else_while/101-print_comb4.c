ede81c03bfb18f79002e0e6ec0e10a2830b9b838
inassdouhdouh <douhdouhinas14@gmail.com>
2023-03-17 16:42:45 -0700

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,6 +9,7 @@
 int main(void)
 
 {
+
 	int digit1, digit2, digit3;
 
 	for (digit1 = 0; digit1 < 8; digit1++)
@@ -23,7 +24,6 @@ int main(void)
 
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
 					continue;
-				
 				putchar(',');
 				putchar(' ');
 			}
