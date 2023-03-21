68413fffac7448072387be9a0e1987afab58caf3
inassdouhdouh <douhdouhinas14@gmail.com>
2023-03-21 07:49:03 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,14 +5,12 @@
  * @c: single letter input
  * Return: 1 if int c is lowercase, 0 if otherwise
  */
-
 int _islower(int c)
 
 {
 
 	if (c >= 'a' && c <= 'z')
         {
-	
 	return (1);
 	}
 	return (0);
