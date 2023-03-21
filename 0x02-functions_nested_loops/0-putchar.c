41942ae76197e7a2ae60f1445e35ed208b43bf01
inassdouhdouh <douhdouhinas14@gmail.com>
2023-03-21 05:35:29 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "Inass.h"
+
+/**
+ * main - Prints "Inass" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_putchar('I');
+	_putchar('n');
+	_putchar('a');
+	_putchar('s');
+	_putchar('s');
+	_putchar('\n');
+
+	return (0);
+}
