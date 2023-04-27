26a18d185720100d82fb45c9c23fb7dde729f4d0
inassdouhdouh <douhdouhinas14@gmail.com>
2023-04-27 08:22:18 -0700

    .

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
 
 size_t print_list(const list_t *h);
 {
-	size_t eside;
+	size_t nelem;
 
-	eside = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		eside++;
+		nelem++;
 	}
-	return (eside);
+	return (nelem);
 }
