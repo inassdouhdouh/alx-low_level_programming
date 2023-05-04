1294476e9c6035cd249be30b887874d62fcae794
inassdouhdouh <douhdouhinas14@gmail.com>
2023-05-04 07:40:04 -0700

    .

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,9 +2,9 @@
 
 /**
  * get_bit -  returns the value of a bit at a given index
- * @n: the number to get bit from
- * @index: the index where the bit get at 
- * Return:0
+ * @n: The number to get bit from
+ * @index: The index where the bit get at
+ * Return: The value of the bit or -1 if an error occured
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
