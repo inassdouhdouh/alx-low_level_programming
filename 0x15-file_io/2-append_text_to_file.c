c86c8468d4952a080621058a7fd9ac5d6ffff4dc
inassdouhdouh <douhdouhinas14@gmail.com>
2023-05-08 08:06:18 -0700

    .

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, a, b = 0;
 
-	if (!filename )
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
