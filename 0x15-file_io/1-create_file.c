47c0b90409e4be2afbbc8bee4ae083149fa4577c
inassdouhdouh <douhdouhinas14@gmail.com>
2023-05-08 07:58:24 -0700

    .

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * create_file - creates a file
+ * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
  *
@@ -14,7 +14,6 @@ int create_file(const char *filename, char *text_content)
 	int o, w, len = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
 
 	if (text_content != NULL)
